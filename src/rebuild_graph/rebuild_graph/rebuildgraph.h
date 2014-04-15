e1ed7579f6aa910ec8b03d81b422d14f938a0f35
oscar-raig <oscar.raig@gmail.com>
2014-04-15 06:19:26 +0200

    Changing names To to temperInitial

---
+++ src/rebuild_graph/rebuild_graph/rebuildgraph.h
@@ -0,0 +1,34 @@
+//
+//  rebuildgraph.h
+//  rebuild_graph
+//
+//  Created by Oscar Raig Colon on 14/04/14.
+//  Copyright (c) 2014 Oscar Raig Colon. All rights reserved.
+//
+
+#ifndef rebuild_graph_rebuildgraph_h
+#define rebuild_graph_rebuildgraph_h
+
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_eigen.h>
+#include <gsl/gsl_sf_exp.h>
+#include <gsl/gsl_linalg.h>
+
+#include "CTrace.hpp"
+
+// Simulated Annealing
+#define TEMPER_INICIAL 10
+#define TMIN 0.0001
+#define NMAX 1000
+#define K 0.9
+#define TOL 0.0001
+#define MAX_ITERATIONS 1000
+#define STRING_LENGTH 256
+
+// Park-Miller pseudo random  number generator
+#define LLAVOR_X 11
+#define LLAVOR_Y 92
+#define LLAVOR_Z 37
+
+
+#endif
