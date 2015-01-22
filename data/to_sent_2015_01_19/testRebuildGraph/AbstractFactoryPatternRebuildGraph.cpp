e5dbc47be60fedd820ea4685b4f7abc63fb8295d
oscar-raig <oscar.raig@gmail.com>
2015-01-23 00:06:09 +0100

    Adding new_send

---
+++ data/to_sent_2015_01_19/testRebuildGraph/AbstractFactoryPatternRebuildGraph.cpp
@@ -0,0 +1,9 @@
+//
+//  AbstractFactoryPatternRebuildGraph.cpp
+//  TestRebuildGraph
+//
+//  Created by Oscar Raig Colon on 26/07/14.
+//  Copyright (c) 2014 Oscar Raig Colon. All rights reserved.
+//
+
+#include "AbstractFactoryPatternRebuildGraph.h"
---
+++ data/to_sent_2015_01_19/testRebuildGraph/analitza.h
@@ -0,0 +1,47 @@
+//
+//  analitza.h
+//  analitza
+//
+//  Created by Oscar Raig Colon on 03/04/14.
+//  Copyright (c) 2014 Oscar Raig Colon. All rights reserved.
+//
+
+#ifndef analitza_analitza_h
+#define analitza_analitza_h
+
+#define VERTEXS 5000
+#define DEGREE 40
+#define MAXTBL 110
+
+/*******************************************************************************
+ ********************************************************************************
+ 
+ DEFINICIO DELS TIPUS I VARIABLES GLOBALS
+ 
+ ********************************************************************************
+ *******************************************************************************/
+
+typedef struct {
+	int     name;               /* Nom                                            */
+	int     degree;             /* Grau                                           */
+	int     nei[DEGREE*3];      /* Veins                                          */
+	float   BC;                 /* Beetweenness Centrality                        */
+	int     nllista;            /* Llista per al c‡lcul de la                     */
+	int     llista[VERTEXS];    /* Beetweenness Centrality                        */
+} vert;
+
+
+
+
+extern vert vertex[VERTEXS];       /* Vector de Vertexs                              */
+extern int dist[VERTEXS][VERTEXS]; /* Matriu de dist‡ncies                           */
+extern int dis[VERTEXS];           /* Vector de dist‡ncies                           */
+extern int Nvertexs;               /* Nombre de Vertexs                              */
+extern const char *nom;                  /* String on guardem el arxiu d¥entrada           */
+
+
+int llegir_dades(const char* nom, int &linies,int &maxveins,int &minveins, float &mitja );
+int distancies();
+int be_ce();
+float clustering(int vert);
+#endif
