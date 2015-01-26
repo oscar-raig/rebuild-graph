8d0247761e91b1cd573daa38ca64b524cedb3184
oscar-raig <oscar.raig@gmail.com>
2015-01-26 01:06:58 +0100

    Refactoring

---
+++ data/to_sent_2015_01_25/rebuild_graph/copyproject_files.sh
@@ -0,0 +1,24 @@
+PROJECT_DIR=/Users/oscarraigcolon/Arrel/git/rebuild-graph/src/rebuild_graph
+
+#cp $PROJECT_DIR/TestRebuildGraph/TestRebuildGraph/AbstractFactoryPatternRebuildGraph.cpp .
+#cp $PROJECT_DIR/TestRebuildGraph/TestRebuildGraph/AbstractFactoryPatternRebuildGraph.h .
+cp $PROJECT_DIR/rebuild_graph/CSettingsSumulation.cpp .
+cp $PROJECT_DIR/rebuild_graph/CSettingsSumulation.h .
+cp $PROJECT_DIR/../../../libcpp/CTrace.cpp .
+cp $PROJECT_DIR/../../../libcpp/CTrace.hpp .
+cp $PROJECT_DIR/rebuild_graph/StrategyPatternAlgorithm.cpp .
+cp $PROJECT_DIR/rebuild_graph/StrategyPatternAlgorithm.h .
+#cp $PROJECT_DIR/TestRebuildGraph/TestRebuildGraph/TestRebuildGraph.cpp .
+#cp $PROJECT_DIR/analitza/analitza/analitza.cpp .
+#cp $PROJECT_DIR/analitza/analitza/analitza.h .
+cp $PROJECT_DIR/graphIndicatorCommunicabilityBetweennessCentrality.h .
+cp $PROJECT_DIR/rebuild_graph/FactoryGraphIndicator.h .
+cp $PROJECT_DIR/rebuild_graph/graphIndicatorBetweennessCentrality.cpp .
+cp $PROJECT_DIR/rebuild_graph/graphIndicatorCommunicabilityCentralityUsingMatrixExponential.h .
+cp $PROJECT_DIR/rebuild_graph/graphIndicatorBetweennessCentrality.h .
+cp $PROJECT_DIR/rebuild_graph/graphIndicator.h .
+cp $PROJECT_DIR/rebuild_graph/gslGraph.cpp .
+cp $PROJECT_DIR/rebuild_graph/gslGraph.h .
+cp $PROJECT_DIR/rebuild_graph/rebuildgraph.cpp .
+cp $PROJECT_DIR/rebuild_graph/rebuildgraph.h .
+cp $PROJECT_DIR/rebuild_graph/main.cpp .
